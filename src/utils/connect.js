48b80fb6826ba82179c4b008afcdc6476b66543b
Promit Saha <promitsaha154@gmail.com>
2025-03-13 21:14:30 +0530

    Minor tweaks

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,6 +1,5 @@
-import { JsonRpcProvider } from 'ethers'; // Correct import for Ethers.js v6
+import { JsonRpcProvider } from 'ethers';
 
-// Load the Base Sepolia RPC URL from the environment variable
 const BASE_SEPOLIA_RPC_URL = process.env.REACT_APP_BASE_SEPOLIA_RPC_URL;
 
 export const getProvider = () => {
